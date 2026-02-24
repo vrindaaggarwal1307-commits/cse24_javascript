0d5cf91dd4a2d022379f829125f568cc9d70a9ce
Vrinda Aggarwal <vrindaaggarwal1307@gmail.com>
2026-02-24 21:57:12 +0530

    assignment operator

---
+++ assignment.js
@@ -0,0 +1,26 @@
+
+var a=10; var b=20;
+var linebreak="</br>";
+
+
+
+document.write("Value of a=>(a=b) =>");
+result =(a =b);
+document.write(result);
+document.write(linebreak);
+
+
+
+document.write("Value of a=>(a+=b) =>");
+result = (a+=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a=>(a-=b) =>");
+result = (a-=b);
+document.write(result);
+document.write(linebreak);
+
+
+
+
