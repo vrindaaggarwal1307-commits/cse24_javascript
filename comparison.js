10f8636686035c703c278d300adf8d88abd8574a
Vrinda Aggarwal <vrindaaggarwal1307@gmail.com>
2026-02-24 21:29:32 +0530

    comparison asg

---
+++ comparison.js
@@ -0,0 +1,30 @@
+var a=10;
+var b=20;
+var linebreak="</br>";
+
+console.log("(a==b)=>");
+result =(a==b);     //returns false
+console.log(result);
+
+console.log("a!=b=>");
+result =(a!=b);
+console.log(result);
+
+console.log("a>b=>");
+result =(a>b);
+console.log(result);
+
+console.log("a<b=>");
+result=(a<b);
+console.log(result);
+
+console.log("a>=b=>");
+result=(a>=b);
+console.log(result);
+
+console.log("a<=b=>");
+resut=(a<=b);
+console.log(result);
+
+
+
