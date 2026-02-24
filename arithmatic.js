fcbac6b484175981086c04baa16d0055e7221b56
Vrinda Aggarwal <vrindaaggarwal1307@gmail.com>
2026-02-24 21:19:33 +0530

    arithmatic assignment

---
+++ arithmatic.js
@@ -0,0 +1,30 @@
+var a=100; var b=13; var linebreak="\n";
+
+
+console.log("a+b");
+result = a+b;
+console.log(result+linebreak);
+
+
+console.log("a*b");
+result = a*b;
+console.log(result+linebreak);
+
+
+
+console.log("a-b");
+result = a-b;
+console.log(result+linebreak);
+
+
+
+console.log("a/b");
+result = a/b;
+console.log(result+linebreak);
+
+
+
+
+
+
+
