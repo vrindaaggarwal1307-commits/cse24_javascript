8b2f2bdea9b777979a376ca75c5b3e9dfbb3dac8
Vrinda Aggarwal <vrindaaggarwal1307@gmail.com>
2026-02-24 22:10:37 +0530

    reference type

---
+++ reference.js
@@ -0,0 +1,21 @@
+ //function
+ var sayHello = function() {
+    alert('Hello World'); }
+    console.log(sayHello);
+
+
+//array
+var numberArray = [1,2,3];
+var animals = new Array('dog', 'cat', 'tiger');
+console.log(numberArray);
+console.log(animals);
+
+//object
+var person = {
+    name: 'Vrinda Aggarwal',
+    age: 20,
+    title: 'Student of ABESEC' 
+}
+
+console.log(person);
+
