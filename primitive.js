08e18d644af539b2813f7212eb699646e913fb19
Vrinda Aggarwal <vrindaaggarwal1307@gmail.com>
2026-02-24 22:02:42 +0530

    primitive type

---
+++ primitive.js
@@ -0,0 +1,19 @@
+var num=10;
+console.log(num);
+console.log(typeof (num));
+
+num = false;
+console.log(num);
+console.log(typeof(num));
+
+num =" Vrinda Aggarwal";
+console.log(num);
+console.log(typeof(num));
+
+num = null;
+console.log(num);
+console.log(typeof(num));
+
+num = undefined;
+console.log(num);
+console.log(typeof(num));
