8881294f02640f08cff043a23d3ec2b6f60e960f
Vrinda Aggarwal <vrindaaggarwal1307@gmail.com>
2026-03-18 19:12:48 +0530

    added new files

---
+++ arrowfunction.js
@@ -0,0 +1,2 @@
+let add = (x,y) => { return x+y; }
+console.log(add(2,3));
---
+++ switch.js
@@ -0,0 +1,20 @@
+var grade='A';
+document.write("Entering switch block<br/>");
+switch(grade) {
+    case 'A':
+        document.write("good job<br/>");
+        break;
+
+         case 'B':
+        document.write("good job<br/>");
+        break;
+
+         case 'C':
+        document.write("good job<br/>");
+        break;
+
+         case 'D':
+        document.write("good job<br/>");
+        break;
+
+}
